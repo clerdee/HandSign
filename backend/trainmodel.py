63fad59e0f22e35124b36c3ae61825d0e0ffd7a7
clerdee <clerdeecruz@gmail.com>
2025-10-22 09:31:03 +0800

    check if may changes

--- backend/trainmodel.py
+++ backend/trainmodel.py
@@ -36,15 +36,6 @@ log_dir = os.path.join('Logs')
 tb_callback = TensorBoard(log_dir=log_dir)
 
 #version 1
-# model = Sequential()
-# model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(30,63)))
-# model.add(LSTM(128, return_sequences=True, activation='relu'))
-# model.add(LSTM(64, return_sequences=False, activation='relu'))
-# model.add(Dense(64, activation='relu'))
-# model.add(Dense(32, activation='relu'))
-# model.add(Dense(actions.shape[0], activation='softmax'))
-
-#version 2
 model = Sequential()
 model.add(LSTM(128, return_sequences=True, activation='relu', input_shape=(None, 63)))
 model.add(LSTM(256, return_sequences=True, activation='relu'))
